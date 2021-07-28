3314f7ff157de3a792b1bc827a2925e420a0d1b1
busyboy-ws <1120344670@qq.com>
2021-07-28 16:25:06 +0800

    add main

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"flag"
+	"path/filepath"
+	utils "watch-dns/pkg/tools"
+	ingress "watch-dns/pkg/controller"
+	"k8s.io/client-go/util/homedir"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"github.com/sirupsen/logrus"
+)
 
 func main() {
-	fmt.Println("starting ...")
+	// load current cluster domain, This is the domain name provided by the cloud vendor.
+	clusterDomain := utils.GetClusterDomain()
+	logrus.Println("current cluster domain: ", clusterDomain)
+	clientSet := getClientSet()
+	ingress.WatchIngressMain(clientSet,clusterDomain)
+}
+
+func getClientSet() *kubernetes.Clientset {
+	if os.Getenv("PHASE") == "prod"{
+			// creates the in-cluster config
+		config, err := rest.InClusterConfig()
+		if err != nil {
+			panic(err.Error())
+		}
+		// creates the clientset
+		clientSet, err := kubernetes.NewForConfig(config)
+		logrus.Printf("run in cluster ...")
+		return clientSet
+	}
+	var kubeConfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeConfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeConfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeConfig)
+	if err != nil {
+		panic(err.Error())
+	}
+	clients, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	logrus.Printf("run in develop...")
+	return clients
 }
--- go.mod
+++ go.mod
@@ -7,4 +7,8 @@ require (
 	github.com/alibabacloud-go/darabonba-openapi v0.1.5
 	github.com/alibabacloud-go/tea v1.1.16
 	github.com/emicklei/go-restful v2.15.0+incompatible
+	github.com/sirupsen/logrus v1.8.1
+	k8s.io/api v0.21.3
+	k8s.io/apimachinery v0.21.3
+	k8s.io/client-go v0.21.3
 )
